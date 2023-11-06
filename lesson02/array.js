0f04052e6f211ef79fc1621c2f3bc4b63b5b237c
Keegan Tews <keegan.tews@yahoo.com>
2023-11-05 20:31:06 -0800

    lesson3 part1

--- lesson02/array.js
+++
@@ -1,8 +0,0 @@
-let arr = ['zero', 'one', 'two', 'three', 'four'];
-console.log(arr);
-arr.push('five');
-console.log(arr);
-arr.splice(2,1);
-console.log(arr);
-let str = arr.join(',');
-console.log(str);
