87b895d89e4cc6cddc1a548c498a650ed7aed28d
Keegan Tews <keegan.tews@yahoo.com>
2023-11-05 16:58:56 -0800

    first challange

---
+++ lesson02/calculator.js
@@ -0,0 +1,15 @@
+let i = 20;
+let j = 10;
+let k = 5;
+let l = 2;
+
+console.log(i+j)
+console.log(i%j)
+console.log(i/k)
+console.log(l*i)
+
+if (i/l === k*l) {
+  console.log(10)
+} else {
+  console.log('fail')
+}
